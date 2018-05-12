d613c8acbb1887c633e6c7517646e390806f767a
lstuiber <lucasstuiber@gmail.com>
2018-05-11 19:50:17 -0500

    added to file
    added some types

---
+++ myTypes.ts
@@ -0,0 +1,16 @@
+export class Category {
+    category: string;
+}
+export class Schedule {
+    id: number;
+    category: string;
+}
+
+export class Account {
+    user_id: string;
+    first_name: string;
+    last_name: string;
+    username: string;
+    phone: string;
+    email: string;
+}
