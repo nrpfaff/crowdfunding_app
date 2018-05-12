bf2ab379fd2b787c83c48de9b0993bcccf30d463
lstuiber <lucasstuiber@gmail.com>
2018-05-11 19:52:35 -0500

    More files
    added code that may or may not work. now its in the right location

--- src/app/myTypes.ts
+++ src/app/myTypes.ts
@@ -5,3 +5,12 @@ export class Schedule {
     id: number;
     category: string;
 }
+
+export class Account {
+    user_id: string;
+    first_name: string;
+    last_name: string;
+    username: string;
+    phone: string;
+    email: string;
+}
